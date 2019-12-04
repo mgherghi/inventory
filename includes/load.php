49a2ec24f4b9987982b44d10466a27a7cae1f413
CoreConduit <bitsandbots@gmail.com>
2019-12-04 14:11:26 -0800

    disabled logging
    // logging disabled ~ remove the comment "//" to enable
    // logAction( $user_id, $remote_ip, $action);

--- includes/load.php
+++ includes/load.php
@@ -21,11 +21,11 @@ require_once(LIB_PATH_INC.'upload.php');
 require_once(LIB_PATH_INC.'database.php');
 require_once(LIB_PATH_INC.'sql.php');
 
-session_start();
 $user_id = $_SESSION['user_id'];
 $remote_ip = $_SERVER['REMOTE_ADDR'];
 $action = $_SERVER['REQUEST_URI'];
 $action = preg_replace('/^.+[\\\\\\/]/', '', $action);
-logAction( $user_id, $remote_ip, $action);
+// logging disabled ~ remove the comment "//" to enable
+// logAction( $user_id, $remote_ip, $action);
 
 ?>
